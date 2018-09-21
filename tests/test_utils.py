dd3d5aa06fa71e6ae655dad965554c85531c524e
007gzs <007gzs@gmail.com>
2018-09-21 12:09:48 +0800

    fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,7 +10,7 @@ class UtilityTestCase(unittest.TestCase):
     def test_object_dict(self):
         obj = ObjectDict()
         self.assertTrue(obj.xxx is None)
-        self.assertTrue(obj['xxx'] is None)
+        self.assertTrue('xxx' not in obj)
         obj.xxx = 1
         self.assertEqual(1, obj.xxx)
         self.assertEqual(obj['xxx'], obj.xxx)
