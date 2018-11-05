29fc7e784f8a9a38c1211f2bf83eba01896edc26
007gzs <007gzs@gmail.com>
2018-11-05 21:02:42 +0800

    pep8

--- tornadoapi/template/jinja2_loader.py
+++ tornadoapi/template/jinja2_loader.py
@@ -4,7 +4,7 @@ import json
 import os
 from jinja2 import Environment, FileSystemLoader, FileSystemBytecodeCache
 from tornado.template import Loader
-from jinja2.defaults import *
+from jinja2 import defaults
 from jinja2.runtime import Undefined
 from tornadoapi.conf import settings
 
@@ -16,18 +16,18 @@ _JINJA_ENV = Environment(bytecode_cache=_CACHE,
                          cache_size=cfg.get('cache_size', 50),
                          auto_reload=cfg.get('filesystem_checks', True),
                          loader=_LOADER,
-                         block_start_string=cfg.get('block_start_string', BLOCK_START_STRING),
-                         block_end_string=cfg.get('block_end_string', BLOCK_END_STRING),
-                         variable_start_string=cfg.get('variable_start_string', VARIABLE_START_STRING),
-                         variable_end_string=cfg.get('variable_end_string', VARIABLE_END_STRING),
-                         comment_start_string=cfg.get('comment_start_string', COMMENT_START_STRING),
-                         comment_end_string=cfg.get('comment_end_string', COMMENT_END_STRING),
-                         line_statement_prefix=cfg.get('line_statement_prefix', LINE_STATEMENT_PREFIX),
-                         line_comment_prefix=cfg.get('line_comment_prefix', LINE_COMMENT_PREFIX),
-                         trim_blocks=cfg.get('trim_blocks', TRIM_BLOCKS),
-                         lstrip_blocks=cfg.get('lstrip_blocks', LSTRIP_BLOCKS),
-                         newline_sequence=cfg.get('newline_sequence', NEWLINE_SEQUENCE),
-                         keep_trailing_newline=cfg.get('keep_trailing_newline', KEEP_TRAILING_NEWLINE),
+                         block_start_string=cfg.get('block_start_string', defaults.BLOCK_START_STRING),
+                         block_end_string=cfg.get('block_end_string', defaults.BLOCK_END_STRING),
+                         variable_start_string=cfg.get('variable_start_string', defaults.VARIABLE_START_STRING),
+                         variable_end_string=cfg.get('variable_end_string', defaults.VARIABLE_END_STRING),
+                         comment_start_string=cfg.get('comment_start_string', defaults.COMMENT_START_STRING),
+                         comment_end_string=cfg.get('comment_end_string', defaults.COMMENT_END_STRING),
+                         line_statement_prefix=cfg.get('line_statement_prefix', defaults.LINE_STATEMENT_PREFIX),
+                         line_comment_prefix=cfg.get('line_comment_prefix', defaults.LINE_COMMENT_PREFIX),
+                         trim_blocks=cfg.get('trim_blocks', defaults.TRIM_BLOCKS),
+                         lstrip_blocks=cfg.get('lstrip_blocks', defaults.LSTRIP_BLOCKS),
+                         newline_sequence=cfg.get('newline_sequence', defaults.NEWLINE_SEQUENCE),
+                         keep_trailing_newline=cfg.get('keep_trailing_newline', defaults.KEEP_TRAILING_NEWLINE),
                          extensions=cfg.get('extensions', ()),
                          optimized=cfg.get('optimized', True),
                          undefined=cfg.get('undefined', Undefined),
